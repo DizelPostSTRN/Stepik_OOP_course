13e59c8a0d6356d86d9f45a9454dcd0ab545d5e8
smaiseyenka <ragno1@mail.ru>
2022-09-03 11:34:53 +0300

    1-4_classes_and_objects_task4 v1

---
+++ 1-4_classes_and_objects_task4.py
@@ -0,0 +1,16 @@
+# Declare a class named Notes and define the following attributes in it:
+# uid: 1005435
+# title: "Шутка"
+# author: "И.С. Бах"
+# pages: 2
+# Then, use the getattr() function to read and display the value of the author attribute.
+
+
+class Notes:
+    uid = 1005435
+    title = "Шутка"
+    author = "И.С. Бах"
+    pages = 2
+
+
+print(getattr(Notes, 'author'))
