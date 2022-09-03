cd548f928f27bd2094892ea30611eaa72646539d
smaiseyenka <ragno1@mail.ru>
2022-09-03 11:18:47 +0300

    1-1_classes_and_objects v1

---
+++ 1-1_classes_and_objects.py
@@ -0,0 +1,15 @@
+# Declare a class named DataBase that holds the following information:
+# pk: 1
+# title: "Классы и объекты"
+# author: "Сергей Балакирев"
+# views: 14356
+# comments: 12
+# Use the same variable (class attribute) names (pk, title, author, views, and comments) with the appropriate values.
+
+
+class DataBase:
+    pk = 1
+    title = "Классы и объекты"
+    author = "Сергей Балакирев"
+    views = 14356
+    comments = 12
